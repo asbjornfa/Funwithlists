94c40ed15c4e1cd0d6bbf82de686c7ebee46bfae
asbjornfa <asbjornfa@gmail.com>
2023-11-21 10:37:22 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[10];
+
+        String[] names = new String[10];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+        //System.out.println(numbers[9]);
+    }
+}
